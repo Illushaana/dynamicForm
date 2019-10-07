9be835f7244c1615d05f913697793c51ee42a0f5
Illushaana <morningarch31@gmail.com>
2019-10-07 18:08:21 +0700

    Add Form & validation

--- app/Http/Controllers/DynamicFieldController.php
+++ app/Http/Controllers/DynamicFieldController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\DynamicField;
 use Validator;
+use Illuminate\Support\Facades\Crypt;
 
 
 class DynamicFieldController extends Controller
@@ -15,23 +16,47 @@ class DynamicFieldController extends Controller
     }
 
     public function addMorePost(Request $request){
-            $rules = [];
-
-            foreach($request->input('name') as $key => $value){
-                $rules["name.{$key}"] ='required';
-            }
-
-            $validator = Validator::make($request->all(), $rules);
-
-            if($validator->passes()){
-
-                foreach($request->input('name') as $key => $value ){
-                    DynamicField::create(['name'=>$value]);
+           
+            if($request->ajax()){
+                $rules = array(
+                        'nama.*' => 'required|min:3|max:30',
+                        'username.*' =>'required|max:30',
+                        'password.*'=>'required',
+                        'email.*'=>'required|email',
+                        'phone.*' => 'required|min:10|numeric|starts_with:+62',
+                        'occupation.*'=>'required'
+                );
+                $error = Validator::make($request->all(),$rules);
+                if($error->fails()){
+                    return response()->json(['error' => $error->errors()->all()
+                    ]);
                 }
-                return response()->json(['success'=>'done']);
+
+                $nama = $request->nama;
+                $username = $request->username;
+                $password = $request->password;
+                $email = $request->email;
+                $phone = $request->phone;
+                $occupation = $request->occupation;
+                
+                for($count = 0; $count <count($nama);$count++){
+                
+                    $data = array(
+                        'nama' => $nama[$count],
+                        'username' => $username[$count],
+                        'password' => $password[$count],
+                        'email' => $email[$count],
+                        'phone' => $phone[$count],
+                        'occupation' => $occupation[$count]
+                    );
+                    $insert_data[] = $data;
+                }
+
+                DynamicField::insert($insert_data);
+                return response ()->json(['success' => 'Data Added Successfully']);
             }
 
-            return response()->json(['error'=>$validator->errors()->all()]);
-    }
+            }
+            
 
 }
